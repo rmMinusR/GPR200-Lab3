5da66b600b42ea2da4fa780c3eefc376f30d3bc6
Sean Sawyers-Abbott <sean.sawyersabbott@mymail.champlain.edu>
2020-10-14 20:09:28 -0400

    Compositing

--- lab6-image.glsl
+++ lab6-image.glsl
@@ -1,6 +1,46 @@
 // GLSL STARTER CODE BY DANIEL S. BUCKSTEIN
 //  -> IMAGE TAB (final)
 
+// https://github.com/CesiumGS/cesium/blob/master/Source/Shaders/Builtin/Functions/luminance.glsl
+// this function was modified from the link above
+float luminance(vec3 rgb)
+{
+    const vec3 W = vec3(0.2125, 0.7154, 0.0721);
+    return dot(rgb, W);
+}
+
+// adds the two sampled textures
+vec4 doAdd(in vec4 x, in vec4 y)
+{
+    return x + y;
+}
+
+// mixes the two sampled textures
+vec4 doMix(in vec4 x, in vec4 y)
+{
+    return mix(x, y, 0.5);
+}
+
+// screens the two sampled textures
+vec4 doScreen(in vec4 x, in vec4 y)
+{
+    return 1. - (1. - x) * (1. - y);
+}
+
+// overlays the two sampled textures
+vec4 doOverlay(in vec4 x, in vec4 y)
+{
+    float lum = luminance(x.rgb);
+    if (lum < 0.5)
+    {
+        return 2. * x * y;
+    }
+    else
+    {
+        return 1. - 2. * (1. - x) * (1. - y);
+    }
+}
+
 //------------------------------------------------------------
 // SHADERTOY MAIN
 
@@ -9,11 +49,8 @@
 //    fragCoord: input location of current pixel in image (in pixels)
 void mainImage(out color4 fragColor, in sCoord fragCoord)
 {
-    // setup
-    // test UV for input image
     sCoord uv = fragCoord / iChannelResolution[0].xy;
-    
-    // TESTING
-    // set iChannel0 to 'Misc/Buffer A' and fetch sample
-    fragColor = texture(iChannel0, uv);
+    vec4 chan0 = texture(iChannel0, uv);
+    vec4 chan1 = texture(iChannel1, uv);
+    fragColor = doMix(chan0, chan1);
 }
