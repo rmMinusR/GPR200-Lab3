cd01cb4bd87ac24bbe12a9987c0a6041348bd84c
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-11-04 15:18:38 -0500

    Implemented shading

--- shaders/LitSpehreFS.glsl
+++ shaders/LitSpehreFS.glsl
@@ -1,9 +1,55 @@
 #version 330
 
+uniform mat4 mViewport;
+
 in vec4 vertColor;
+in vec4 vertPos;
+in vec4 vertNormal;
 
 out vec4 fragColor;
 
+struct Light {
+	vec4 position;
+	vec3 color;
+	float intensity;
+};
+
+float sq(float v) { return v*v; }
+
+vec3 lambert(in vec4 lightVector, in vec4 normal, in vec3 diffuse_color, in Light light, in float d) {
+	float diffuse_coeff = max(0., dot(normal, lightVector));
+	float attenuated_intensity = 1./sq(d/light.intensity+1.);
+	float diffuse_intensity = diffuse_coeff * attenuated_intensity;
+	
+	return diffuse_coeff*diffuse_color*light.color;
+}
+
+vec3 phong(vec4 view_pos, vec4 pos, vec4 normal, vec3 diffuse_color, vec3 spec_color, in Light light) {
+	vec4 viewVector = normalize(view_pos-pos);
+	vec4 lightVector = light.position-pos;
+	float lightVecLength = length(lightVector);
+	lightVector /= lightVecLength;
+	vec4 reflectedLightVector = reflect(-lightVector, normal);
+	
+	float ks = max(0., dot(viewVector, reflectedLightVector));
+	
+	float spec_intensity = pow(ks, 4);
+	
+	vec3 lambert_reflect = lambert(lightVector, normal, diffuse_color, light, lightVecLength);
+	
+	return vec3(0) + (lambert_reflect + spec_intensity*spec_color) * light.color;
+}
+
 void main() {
-	fragColor = vertColor;
+	vec4 vp_pos = vec4(mViewport[0].w, mViewport[1].w, mViewport[2].w, 1.);
+	
+	Light light;
+	light.position = vec4(1, 2, 3, 1);
+	light.color = vec3(0, 0.5, 1);
+	light.intensity = 4.;
+	
+	vec3 diffuse_color = vec3(1);
+	vec3 spec_color = vec3(1);
+	
+	fragColor = vec4( phong(vp_pos, vertPos, vertNormal, diffuse_color, spec_color, light), 1);
 }
--- shaders/LitSphereVS.glsl
+++ shaders/LitSphereVS.glsl
@@ -1,7 +1,7 @@
 #version 330
 
 #extension GL_GOOGLE_include_directive : enable
-//#include "test.glsl"
+//#include </common.glsl>
 
 #ifdef GL_ES
 precision highp float;
@@ -10,14 +10,19 @@ precision highp float;
 uniform mat4 mViewport;
 uniform mat4 mModel;
 
-layout (location = 0) in vec3 pos;
-layout (location = 1) in vec3 normal;
+layout (location = 0) in vec4 pos;
+layout (location = 1) in vec4 normal;
 
 layout (location = 2) in vec2 uv;
 
 out vec4 vertColor;
+out vec4 vertPos;
+out vec4 vertNormal;
+out vec2 vertUv;
 
 void main() {
-	vertColor = vec4(0.5+0.5*normal, 1.);
-	gl_Position = mViewport * mModel * vec4(pos, 1.);
+	vertColor = vec4(0.5+0.5*normal.xyz, 1.);
+	vertPos = mModel * pos;
+	vertNormal = mModel * normal;
+	gl_Position = mViewport * mModel * pos;
 }
