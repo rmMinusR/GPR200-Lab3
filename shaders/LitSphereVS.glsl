b5ded031c17f88fdf779a0e9279a71b544263348
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-11-04 13:36:07 -0500

    Setup UV

--- shaders/LitSphereVS.glsl
+++ shaders/LitSphereVS.glsl
@@ -1,10 +1,19 @@
 #version 330
 
+#extension GL_GOOGLE_include_directive : enable
+//#include "test.glsl"
+
+#ifdef GL_ES
+precision highp float;
+#endif//GL_ES
+
 uniform mat4 mViewport;
 uniform mat4 mModel;
 
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec3 normal;
+
+layout (location = 2) in vec2 uv;
 
 out vec4 vertColor;
 
