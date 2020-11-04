38d3113ecb0656ec9013cd1047f4ffed3e27b7d2
Sean Sawyers-Abbott <sean.sawyersabbott@mymail.champlain.edu>
2020-11-04 16:28:30 -0500

    Added a lot of lighting and whatnot

--- shaders/LitSpehreFS.glsl
+++ shaders/LitSpehreFS.glsl
@@ -2,54 +2,17 @@
 
 uniform mat4 mViewport;
 
-in vec4 vertColor;
-in vec4 vertPos;
-in vec4 vertNormal;
+uniform sampler2D texDiff;
+uniform sampler2D texSpec;
+
+in vec3 specIntensity;
+in vec3 diffuseIntensity;
+in vec2 vecUv;
 
 out vec4 fragColor;
 
-struct Light {
-	vec4 position;
-	vec3 color;
-	float intensity;
-};
-
-float sq(float v) { return v*v; }
-
-vec3 lambert(in vec4 lightVector, in vec4 normal, in vec3 diffuse_color, in Light light, in float d) {
-	float diffuse_coeff = max(0., dot(normal, lightVector));
-	float attenuated_intensity = 1./sq(d/light.intensity+1.);
-	float diffuse_intensity = diffuse_coeff * attenuated_intensity;
-	
-	return diffuse_coeff*diffuse_color*light.color;
-}
-
-vec3 phong(vec4 view_pos, vec4 pos, vec4 normal, vec3 diffuse_color, vec3 spec_color, in Light light) {
-	vec4 viewVector = normalize(view_pos-pos);
-	vec4 lightVector = light.position-pos;
-	float lightVecLength = length(lightVector);
-	lightVector /= lightVecLength;
-	vec4 reflectedLightVector = reflect(-lightVector, normal);
-	
-	float ks = max(0., dot(viewVector, reflectedLightVector));
-	
-	float spec_intensity = pow(ks, 4);
-	
-	vec3 lambert_reflect = lambert(lightVector, normal, diffuse_color, light, lightVecLength);
-	
-	return vec3(0) + (lambert_reflect + spec_intensity*spec_color) * light.color;
-}
-
 void main() {
-	vec4 vp_pos = vec4(mViewport[0].w, mViewport[1].w, mViewport[2].w, 1.);
-	
-	Light light;
-	light.position = vec4(1, 2, 3, 1);
-	light.color = vec3(0, 0.5, 1);
-	light.intensity = 4.;
-	
-	vec3 diffuse_color = vec3(1);
-	vec3 spec_color = vec3(1);
-	
-	fragColor = vec4( phong(vp_pos, vertPos, vertNormal, diffuse_color, spec_color, light), 1);
+	vec3 diffuse_color = texture(texDiff, vecUv).rgb;
+	vec3 spec_color = texture(texSpec, vecUv).rgb;
+	fragColor = vec4(vec3(0) + ((diffuseIntensity * diffuse_color) + (specIntensity * spec_color)), 1);
 }
--- shaders/LitSphereVS.glsl
+++ shaders/LitSphereVS.glsl
@@ -15,14 +15,52 @@ layout (location = 1) in vec4 normal;
 
 layout (location = 2) in vec2 uv;
 
-out vec4 vertColor;
-out vec4 vertPos;
-out vec4 vertNormal;
-out vec2 vertUv;
+out vec3 specIntensity;
+out vec3 diffuseIntensity;
+out vec2 vecUv;
+
+struct Light {
+	vec4 position;
+	vec3 color;
+	float intensity;
+};
+
+float sq(float v) { return v*v; }
+
+float lambert(in vec4 lightVector, in vec4 normal, in Light light, in float d) {
+	float diffuse_coeff = max(0., dot(normal, lightVector));
+	float attenuated_intensity = 1./sq(d/light.intensity+1.);
+	return diffuse_coeff * attenuated_intensity;
+}
+
+void phong(out vec3 specIntensity, out vec3 diffuseIntensity, vec4 view_pos, vec4 pos, vec4 normal, in Light light) {
+	vec4 viewVector = normalize(view_pos-pos);
+	vec4 lightVector = light.position-pos;
+	float lightVecLength = length(lightVector);
+	lightVector /= lightVecLength;
+	vec4 reflectedLightVector = reflect(lightVector, normal);
+	
+	float ks = max(0., dot(viewVector, reflectedLightVector));
+	
+	specIntensity = vec3(pow(ks, 4));
+	
+	diffuseIntensity = vec3(lambert(lightVector, normal, light, lightVecLength));
+}
 
 void main() {
-	vertColor = vec4(0.5+0.5*normal.xyz, 1.);
-	vertPos = mModel * pos;
-	vertNormal = mModel * normal;
+	vec4 vp_pos = vec4(mViewport[0].w, mViewport[1].w, mViewport[2].w, 1.);
+	
+	Light light;
+	light.position = vec4(1, 2, 3, 1);
+	light.color = vec3(1, 1, 1);
+	light.intensity = 4.;
+	
+	phong(specIntensity, diffuseIntensity, vp_pos, pos, normal, light);
+	
+	specIntensity *= light.color;
+	diffuseIntensity *= light.color;
+	
+	vecUv = uv;
+	
 	gl_Position = mViewport * mModel * pos;
 }
