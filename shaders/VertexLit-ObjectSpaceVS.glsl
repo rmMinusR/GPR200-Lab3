d3c117b4d6ba32bfdd7b2b6472428b8d22af5050
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-11-06 12:19:43 -0500

    Final optimization tweaks

--- shaders/VertexLit-ObjectSpaceVS.glsl
+++ shaders/VertexLit-ObjectSpaceVS.glsl
@@ -26,6 +26,42 @@ uniform Light light0, light1, light2;
 
 float sq(float v) { return v*v; }
 
+//Integer-power. Significantly faster than pow(float, float)
+float ipow(float b, int x) {
+	/*
+		b^x =
+		b^(x_0+x_1+...+x_n) =
+		b^x_0 * b^x_1 * ... * b^x_n
+		x_k represents a bitmasked x where only the k-th bit is passed through
+	*/
+    //Acts as a stack representing x, will be interpreted one bit at a time LSB first
+    uint powstack = uint(x);
+    
+    //Will always be b raised to some power that is a power of two
+    //(p^q)^2 = p^2q
+    float p2 = b;
+    
+    //The output b^x_0 * ... * b^x_k
+    float val = 1.;
+    
+    //Loop until we've reached the last bit (will always be most-significant)
+    while(powstack != uint(0)) {
+        //Pop a bit from remaining power stack
+        bool poppedBit = (powstack&uint(1)) != uint(0);
+        powstack = powstack >> 1;
+        
+        //Update output value
+        if(poppedBit) val *= p2;
+        
+        //Powers of b raised to a power of two.
+        //(p^q)^2 = p^2q
+        //(p^2^k)^2 = p^(2*2^k) = p^2^(k+1)
+        p2 *= p2;
+    }
+    
+    return val;
+}
+
 // returns the diffuse intensity
 float lambert(in vec4 lightVector, in vec4 normal, in Light light, in float d) {
 	float diffuse_coeff = max(0., dot(normal, lightVector));
@@ -37,7 +73,7 @@ float lambert(in vec4 lightVector, in vec4 normal, in Light light, in float d) {
 float phong(vec4 lightVector, vec4 viewVector, vec4 normal, in Light light) {
 	vec4 reflectedLightVector = reflect(lightVector, normal);
 	float ks = max(0., dot(viewVector, reflectedLightVector));
-	return pow(ks, 2);
+	return ipow(ks, 2);
 }
 
 void main() {
