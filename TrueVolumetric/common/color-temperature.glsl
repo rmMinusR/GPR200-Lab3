e4c19cd1fd7c0aa819b43bf84d6cee1ecfc98607
Robert Christensen <robert.christensen@mymail.champlain.edu>
2020-12-15 03:54:00 -0500

    Added color temperature utils (for fire), fixed occlusion issues

---
+++ TrueVolumetric/common/color-temperature.glsl
@@ -0,0 +1,46 @@
+#ifndef HG_COLOR_TEMPERATURE
+#define HG_COLOR_TEMPERATURE
+
+/*
+
+Credit goes to Tanner Helland for fitting the data and writing the psuedocode / Visual Basic code:
+https://tannerhelland.com/2012/09/18/convert-temperature-rgb-algorithm-code.html
+
+And to Renaud Bédard for porting it to GLSL: https://www.shadertoy.com/view/lsSXW1
+
+*/
+
+float saturate(float v) { return clamp(v, 0.0,       1.0);       }
+vec2  saturate(vec2  v) { return clamp(v, vec2(0.0), vec2(1.0)); }
+vec3  saturate(vec3  v) { return clamp(v, vec3(0.0), vec3(1.0)); }
+vec4  saturate(vec4  v) { return clamp(v, vec4(0.0), vec4(1.0)); }
+
+vec3 ColorTemperatureToRGB(float temperatureInKelvins)
+{
+	vec3 retColor;
+	
+    temperatureInKelvins = clamp(temperatureInKelvins, 1000.0, 40000.0) / 100.0;
+    
+    if (temperatureInKelvins <= 66.0)
+    {
+        retColor.r = 1.0;
+        retColor.g = saturate(0.39008157876901960784 * log(temperatureInKelvins) - 0.63184144378862745098);
+    }
+    else
+    {
+    	float t = temperatureInKelvins - 60.0;
+        retColor.r = saturate(1.29293618606274509804 * pow(t, -0.1332047592));
+        retColor.g = saturate(1.12989086089529411765 * pow(t, -0.0755148492));
+    }
+    
+    if (temperatureInKelvins >= 66.0)
+        retColor.b = 1.0;
+    else if(temperatureInKelvins <= 19.0)
+        retColor.b = 0.0;
+    else
+        retColor.b = saturate(0.54320678911019607843 * log(temperatureInKelvins - 10.0) - 1.19625408914);
+
+    return retColor;
+}
+
+#endif
